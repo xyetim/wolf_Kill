aa651abf4ea494b7379e42628f99a3b038ad6798
ysho123 <569513303@qq.com>
2018-03-14 07:55:44 +0800

    1
    1

--- WX_wolfKill/pages/settingRoom/setting.js
+++ WX_wolfKill/pages/settingRoom/setting.js
@@ -4,48 +4,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    sums:[8,9,10,11,12],
-    gods:[
-      {name:"预言家",value:"YYJ"},
-      {name:"女巫",value:"NW",checked:1},
-      {name:"猎人",value:"LR",checked:1},
-      {name:"守卫",value:"SW"},
-      {name:"白痴",value:"BC"}
-    ],
-    wolves:[
-      {name:"2狼",value:"2"},
-      {name:"3狼",value:"3"},
-      {name:"4狼",value:"4"}
-    ],
-    police:[
-      {name:"是",value:"Y"},
-      {name:"否",value:"N"}
-    ],
-    sumPeople:0,
-    godsPeople:0,
-    wolfPeople:0,
-    farmerPeople:0
+    items: [
+      { name: 'USA', value: '美国' },
+      { name: 'CHN', value: '中国', checked: 'true' },
+      { name: 'BRA', value: '巴西' },
+      { name: 'JPN', value: '日本' },
+      { name: 'ENG', value: '英国' },
+      { name: 'TUR', value: '法国' },
+    ]
   },
-
-  pickerChange: function (e) {
-    var index = e.detail.value;
-    var sum = this.data.sums[index];
-    this.setData({
-      sumPeople : sum
-    });
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
   },
-
-  checkboxChange: function(e){
-    var god = e.detail.value;
-    godsPeople = god;
-    console.log(godsPeople);
-  },
-
-  formsubmit : function(e){
-    var formSet = {};
-    formSet = e.detail.value;
-    console.log(formSet);
-  }
-
-
 })
